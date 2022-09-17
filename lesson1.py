2e751a6ba5c32897a14b031cb3e7896e36d8bd08
ivan <i.s.perminov@yandex.ru>
2022-09-17 11:14:44 +0700

    Finished write lesson1.py and wrote fist chapter in book

--- lesson1.py
+++ lesson1.py
@@ -8,6 +8,16 @@ print ('privet')
 #репликация строк 'abc' * 2 -> abcabc
 #переменная - область памяти компьютера, в которой может храниться одиночное значение
 #инициализация, перезапись переменных, имена переменых чуствительны к регистру, имена переменных должны начинаться с маленькой буквы
-#хороший стиль вместо looking_like_this  lookingLikeThis
-#
-####################
+#используем стиль вместо looking_like_this  lookingLikeThis
+#функция print() -  без аргументов
+#input() - всегда возвращает строку
+###################
+print('Hello world')
+print('What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The lenght of you name is:')
+print(len(myName))
+print('Wath is you age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
