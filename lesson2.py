ffe27b38d359d2b4ea8a0b3ef51cc23c9b5565bf
ivan <i.s.perminov@yandex.ru>
2022-09-18 19:03:28 +0700

    Add file lesson2.py

---
+++ lesson2.py
@@ -0,0 +1,47 @@
+#lesson2
+#True and False всегда с большой буквы
+# ==  != 
+#and or - работают всегда с 2-мя булевыми значениями или выражениями
+#not - воздействует только на одно булево выражение, следовательно является унарным
+#>>> (4<5) and (5<6)
+#True
+'''приоритет 1. not, 2. and, 3. or
+           if else
+if name == 'Alice':
+   print('Hi, Alice')
+else:
+   print('Hello, stranger.')
+
+           elif -  помещается только после инструкции if или другой инструкции elif. проверяется лишь в том
+случае, если все предыдущие условия оказались ложными, а если оно истинно, то остальные блоки elifпропустятся и выполнится блок else 
+if name == 'Alice':
+   print('Hi, Alice')
+elif age < 12:
+    print('You are not Alice.')
+else:
+   print('Hello, stranger.')
+############'''
+#
+"""
+"""
+
+#инструкция while - цикл выполняется до тех пор пока выполняется указанное условие 
+spam = 0
+while spam < 5:
+    print('Hello, world.')
+    spam = spam + 1
+
+#break - условие выхода из цикла
+'''
+while True:
+    print('Please type your name.')
+    name = input(0
+    if name == 'you name':
+        break
+print('Thank you!')
+'''
+
+#continue
+
+
+
