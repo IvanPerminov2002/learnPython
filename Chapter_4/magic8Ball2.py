37ab6c686c5eff380928a101dd662fdad523bca2
ivan <i.s.perminov@yandex.ru>
2022-10-01 12:12:26 +0700

    write lesson4.py, wrote magic8Ball2.py

---
+++ Chapter_4/magic8Ball2.py
@@ -0,0 +1,5 @@
+import random
+
+messeges = ['It is certain', 'It is decidedly so', 'Yes', 'Reply hazy try again', 'Ask again later', 'Concentrate and ask again', 'My reply is no', 'Outlook not so good', 'Very doubtful']
+
+print(messeges[random.randint(0, len(messeges) - 1)])
